853de852f68c5bcb5e0ab09988ee77db050a856b
A.P. Liang <apandaliang@gmail.com>
2016-09-09 12:30:14 +0800

    Initial

---
+++ Coin.java
@@ -0,0 +1,15 @@
+import awt.Image;
+import java.awt.Graphics
+
+
+public class Coin {
+private Image heads;
+private Image tails;
+
+	public Coin(Image headsImg, Image tailsImg){
+		heads = headsImg;
+		tail = tailsImg;
+	}
+	
+	
+}
